de515639c85ffa1287907b540987a57b8fb1a425
Justin Simon <justinsimoncs@gmail.com>
2025-11-30 08:15:23 -0800

    Finished requirments for step 1

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        public Department GetAllRepositories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertDepository()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
